93803001f3b11cebc5729886252410afb034b66e
Paola L <paolalarrazabal@gmail.com>
2019-10-19 23:59:00 -0400

    added objects and deleted click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,55 @@
 
-
+$(document).ready(function() {
 
 let number = 180
 let countdown;
-let right = 0;
-let wrong = 0; 
 
 
 
-console.log(wrong);
-console.log(right);
+let trivia = {
+right: 0, 
+wrong: 0, 
 
 
-$('#wrongAnswer').on('click',function () {
-wrong++;
+questions: {
+    question1: "What is the name of the hotel in the movie The Shining?",
+    question2: "Who has murdered the most people in a slasher film?",
+    question3: "Which actor played Pennywise in the 1990 miniseries?",
+    question4: "Which horror movie was the first to be nominated for an oscar?"
+}
+,
+choices: {
+    question1: ["The Stanley Hotel", "Timberline Lodge", "Ahwahnee Hotel", "Overlook Hotel"],
+    question2: ["Jason Voorhees", "Freddy Krueger", "Micheal Meyers", "Leatherface"],
+    question3: ["Alice Cooper","Roddy McDowall", "Malcolm McDowell","Tim Curry"],
+    question4: ["The Omen", "The Exorcist", "The Shining", "The Silence of the Lambs"],
+}
+,
+rightAnswers: {
+    question1: "Overlook Hotel",
+    question2:"Jason Voorhees",
+    question3:"Tim Curry",
+    question4: "The Exorcist",
+}
+}
 
-});
-
-$('#rightAnswer').on('click',function() {
-right++;
-
-});
 
 
 
-function showtriviaPage() {
+
+
+
+
+
+
+
+
+$('#showQuestions').on('click',function(){
     document.getElementById('start-button').style.display = "none"; 
     document.getElementById('trivia-page').style.display = "block";
     countdown = setInterval(decrement, 1000);
 
-}
+});
 
 function decrement() { 
     number--;
@@ -45,3 +65,15 @@ function decrement() {
 function stop () {
     clearInterval(countdown);
 }
+
+
+
+
+
+
+
+
+
+});
+
+
